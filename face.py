f8bef312532fe466b4e85e17f2f13602a6def414
keno-tech <techin.keno2@gmail.com>
2023-06-11 17:11:12 +1000

    error checking

--- face.py
+++ face.py
@@ -52,65 +52,72 @@ def main():
     # Load video using OpenCV
     video = cv2.VideoCapture(CAMERA)
 
-    while True:
-        ret, frame = video.read()
+    try:
+        while True:
+            ret, frame = video.read()
 
-        if not ret:
-            break
-        frame = cv2.flip(frame, 1)
-        # Convert the BGR image to RGB
-        rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        h, w, _ = frame.shape
+            if not ret:
+                break
+            frame = cv2.flip(frame, 1)
+            # Convert the BGR image to RGB
+            rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            h, w, _ = frame.shape
 
-        # Process the frame with Mediapipe face detection
-        results = face_detection.process(rgb_frame)
+            # Process the frame with Mediapipe face detection
+            results = face_detection.process(rgb_frame)
 
-        left_start_point = (int(w * threshold), h)
-        left_end_point = (int(w * threshold), 0)
-        right_start_point = (int(w * (1 - threshold)), h)
-        right_end_point = (int(w * (1 - threshold)), 0)
+            left_start_point = (int(w * threshold), h)
+            left_end_point = (int(w * threshold), 0)
+            right_start_point = (int(w * (1 - threshold)), h)
+            right_end_point = (int(w * (1 - threshold)), 0)
 
-        cv2.line(frame, left_start_point, left_end_point, (0, 255, 0), 2)
-        cv2.line(frame, right_start_point, right_end_point, (0, 255, 0), 2)
+            cv2.line(frame, left_start_point, left_end_point, (0, 255, 0), 2)
+            cv2.line(frame, right_start_point, right_end_point, (0, 255, 0), 2)
 
-        if results.detections:
-            for detection in results.detections:
-                # Extract the bounding box coordinates
-                bbox = detection.location_data.relative_bounding_box
-                h, w, _ = frame.shape
+            if results.detections:
+                for detection in results.detections:
+                    # Extract the bounding box coordinates
+                    bbox = detection.location_data.relative_bounding_box
+                    h, w, _ = frame.shape
 
-                # Convert relative coordinates to absolute coordinates
-                xmin = int(bbox.xmin * w)
-                ymin = int(bbox.ymin * h)
-                bbox_width = int(bbox.width * w)
-                bbox_height = int(bbox.height * h)
-                xmax = xmin + bbox_width
-                ymax = ymin + bbox_height
-                
-                # Draw bounding box rectangle and label on the frame
-                cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), (0, 255, 0), 2)
-                cv2.putText(frame, 'Face', (xmin, ymin - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+                    # Convert relative coordinates to absolute coordinates
+                    xmin = int(bbox.xmin * w)
+                    ymin = int(bbox.ymin * h)
+                    bbox_width = int(bbox.width * w)
+                    bbox_height = int(bbox.height * h)
+                    xmax = xmin + bbox_width
+                    ymax = ymin + bbox_height
+                    
+                    # Draw bounding box rectangle and label on the frame
+                    cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), (0, 255, 0), 2)
+                    cv2.putText(frame, 'Face', (xmin, ymin - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
 
-                left_start_point, left_end_point, = int(w * threshold), int(w * (1 - threshold))
+                    left_start_point, left_end_point, = int(w * threshold), int(w * (1 - threshold))
 
-                if left_start_point > xmin and left_end_point < xmax:
-                    continue
+                    if left_start_point > xmin and left_end_point < xmax:
+                        continue
 
-                if xmin < left_start_point:
-                    pyautogui.scroll(speed)
-                elif xmax > left_end_point:
-                    pyautogui.scroll(-speed)
+                    if xmin < left_start_point:
+                        pyautogui.scroll(speed)
+                    elif xmax > left_end_point:
+                        pyautogui.scroll(-speed)
 
-        # Display the frame
-        cv2.imshow('Face Detection', frame)
+            # Display the frame
+            cv2.imshow('Face Detection', frame)
+            
+
+            if cv2.waitKey(10) & 0xFF == ESCAPE_BUTTON or cv2.getWindowProperty('Face Detection', cv2.WND_PROP_VISIBLE) < 1:
+                break
+
+        # Release the video capture and close all windows
         
+    except Exception as e:
+        print(f'Error during face detection: {str(e)}')
+    
+    finally:
+        video.release()
+        cv2.destroyAllWindows()
 
-        if cv2.waitKey(10) & 0xFF == ESCAPE_BUTTON or cv2.getWindowProperty('Face Detection', cv2.WND_PROP_VISIBLE) < 1:
-            break
-
-    # Release the video capture and close all windows
-    video.release()
-    cv2.destroyAllWindows()
 
 def update_threshold(value):
     global threshold
@@ -222,10 +229,12 @@ class ConfigWindow(QWidget):
 
 
 if __name__ == "__main__":
-    app = QApplication([])
-    config_window = ConfigWindow()
-    config_window.show()
-    app.setStyle("Fusion")
-    app.exec_()
-
+    try:
+        app = QApplication([])
+        config_window = ConfigWindow()
+        config_window.show()
+        app.setStyle("Fusion")
+        app.exec_()
+    except Exception as e:
+        print(f'Error during application exec: {str(e)}')
 
